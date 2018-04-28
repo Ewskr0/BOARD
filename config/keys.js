7c03e1bdc2047cbf52116496f080c562a4b06e1d
Ewskr0 <ewann.villa@gmail.com>
2018-04-28 12:06:42 +0200

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,12 +1,12 @@
 module.exports = {
   google: {
-    clientID:'1010575445501-jqh3pucfvdskp5qv0pmjp8g26kbhl1ie.apps.googleusercontent.com',
-    clientSecret:'c5TWYTipTCeopH6rNb7rVd1D'
+    clientID:'to change',
+    clientSecret:'to change'
   },
   mongodb: {
-    dbURI: 'mongodb://nnawe:123mdp@ds159459.mlab.com:59459/adminboard-auth'
+    dbURI: 'to change'
   },
   cookieKey:{
-    key: 'niuIUKWjwnJQqNDJWMWSWJQnqnqj'
+    key: 'to change'
   } 
 }
